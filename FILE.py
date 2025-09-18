dd09d8de369f8d90cfafd697e2e9fe3fe0c125ff
Dani Malik <112042251+DaniMalik1@users.noreply.github.com>
2025-09-18 16:30:32 +0500

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -1,1 +1,23 @@
-import dumpworking_enc
+#####apni 
+ #coding=utf-8
+import os, sys, platform
+ 
+os.system('git pull')
+ 
+try:
+    if sys.argv[1]=='update':
+        os.system('git pull')
+except:
+    pass
+ 
+ 
+bit = platform.architecture()[0]
+if bit == '64bit':
+    
+        import dumpworking_enc
+    
+ 
+elif bit == '32bit':
+    
+        import dumpworking1_enc
+ 
